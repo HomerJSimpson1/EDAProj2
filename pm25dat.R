f0770f8fc4528fa502ff5c6d1ca0a473740e8acd
HomerJSimpson1 <vuke93@gmx.com>
2014-07-21 18:28:45 -0400

    First draft

---
+++ pm25dat.R
@@ -0,0 +1,133 @@
+pm25dat <- function(strFilePath, DOWNLD_UNZIP = TRUE, USE_INET2 = FALSE) {
+  ## Exploratory Data Analysis Coursera Class
+  ## Project 2
+  ## 
+
+
+  ## Read the data into R.
+  readdata(strFilePath, DOWNLD_UNZIP, USE_INET2)
+
+  ## Use merge() to join the two data frames.
+
+
+
+  ## Return the merged data frames?
+}
+
+
+
+readdata <- function(strFilePath, DOWNLD_UNZIP, USE_INET2) {
+  ## Flag to determine if I should download and unzip the data or not.
+  #DOWNLD_UNZIP <- FALSE
+  #DOWNLD_UNZIP <- TRUE
+
+  ## Flag to determine if I need to use setInternet2(use=TRUE)
+  #USE_INET2 <- FALSE
+  #USE_INET2 <- TRUE
+
+  ## Set the values of some directories and filenames that will be used throughout.
+  #destdir <- "C:/Users/petersj/My Documents/data"
+  ## if (!file.exists("C:/temp"))
+  ##   {
+  ##     dir.create("C:/temp")
+  ##   }
+
+  #destdir <- "C:/Temp/data"
+  destdir <- getwd()
+  dstfile <- paste(destdir, "/exdata_data_NEI_data.zip", sep="")
+  unzipdst <- paste(destdir, "/unzipped", sep="")
+  
+  if (DOWNLD_UNZIP)
+    {
+      ## Used to determine if I changed the setInternet2 value in the call to the useinet2() function.  i.e. it may
+      ## already have been set to setInternet2(use=TRUE), prior to calling the useinet2() function.
+      changed = FALSE
+      if (USE_INET2)
+        { 
+          changed = useinet2(changed)
+        }
+  
+      ## Download data from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+      fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+      datadownld(fileURL, destdir, dstfile)
+      
+      ## Unzip the file(s)
+      unzipthedata(destdir, dstfile)
+
+      ## Not sure if I need to do the following, or if it even raises an error.
+      if (USE_INET2 && changed)
+        {
+          setInternet2(use=FALSE)
+        }
+    }
+
+  ## ## Examine the files
+  ## ## Top level directory for the unzipped files is now unzipdst + "UCI HAR Dataset"
+  ## filelst <- list.files(paste(unzipdst, "/UCI HAR Dataset", sep=""))
+  ## filelst
+
+  ## ## Read the files into R (there are 2 files).  The files are stored as .rds files, which are read into R
+  ## ## using the readRDS function (and written using the saveRDS function).
+  ## NEI <- readRDS("summarySCC_PM25.rds")
+  ## SCC <- readRDS("Source_Classification_Code.rds")
+  ## print(str(NEI))
+  ## print(str(SCC))  
+
+}
+
+
+datadownld <- function(fileURL, destdir, dstfile) {
+  ## Download the data
+  ## First, set the URL value.
+  ## e.g. fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+  print(destdir)
+  print(dstfile)
+  print(fileURL)
+  
+  ## If the directory where we're going to put the data doesn't exist, create it.
+  if (!file.exists(destdir))
+    {
+      dir.create(destdir)
+    }
+
+  ## Call download.file().  If the URL is secure (https), then on some systems you need to specify method="curl".
+  ## Don't use method="curl" on Windows when setInternet2 is TRUE.
+  # download.file(fileURL, destfile = dstfile, method="curl")
+
+  if(substr(fileURL, 1, 5) == "https")
+    download.file(fileURL, destfile = dstfile, method="curl")
+  else if(substr(fileURL, 1, 5) == "http:")
+    download.file(fileURL, destfile = dstfile)
+
+  dateDownloaded <- date()  
+}
+
+
+useinet2 <- function(changed) {
+  ## Only need this at work, not at home.
+  ## setInternet2(use=TRUE) causes R to load the internet2.dll file
+  ## Use setInternet2(NA) to return the current setting.  If FALSE, change to true.
+  if (!setInternet2(NA))
+    {
+      print("Changing setInternet2 setting")
+      setInternet2(use=TRUE)
+      changed = TRUE
+    }
+  return(changed)
+}
+
+
+unzipthedata <- function(datadir, datafile) {
+  ## The directory to which I will unzip
+  unzipdst <- paste(datadir, "/unzipped", sep="")
+
+  ## If this directory does not already exist, create it.
+  if (!file.exists(unzipdst))
+    {
+      dir.create(unzipdst)
+    }
+  
+  ## Unzip the file
+  unzip(datafile, exdir=unzipdst)  
+}
